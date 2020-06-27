d5691d295e4cdd9861615ec90e37aa87d710683f
yunan.zhang <zhangyunan@91jinrong.com>
2020-06-27 19:35:31 +0800

    添加axios

---
+++ src/assets/utils/cookie.js
@@ -0,0 +1,27 @@
+let cookie = {
+  setCookie (cname, value, expiredays) {
+    let exdate = new Date()
+    exdate.setTime(exdate.getTime() + expiredays)
+    exdate.setDate(exdate.getDate() + expiredays)
+    document.cookie = cname + '=' + escape(value) + ((expiredays == null) ? '' : ';expires=' + exdate.toGMTString())
+  },
+  getCookie (name) {
+    let reg = new RegExp('(^| )' + name + '=([^;]*)(;|$)')
+    let arr = document.cookie.match(reg)
+    if (arr) {
+      return (arr[2])
+    } else {
+      return null
+    }
+  },
+  delCookie (name) {
+    let exp = new Date()
+    exp.setTime(exp.getTime() - 1)
+    let cval = cookie.getCookie(name)
+    if (cval != null) {
+      document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;'
+    }
+  }
+}
+
+export default cookie
